28e5b5690ead2d96b37f61ab944ff555385427c6
Mamdouh Awny <mamdouhawny8@gmail.com>
2019-04-12 21:31:23 +0200

    need contriputers

---
+++ app.js
@@ -0,0 +1,15 @@
+function myFunction(){
+var popup=  document.getElementById('best').onload="bitter";
+}
+
+
+function myPass(){
+  var firstVar=document.getElementById("pass");
+  var secVar=document.getElementById("word");
+
+  if (firstVar==secVar) {
+    alert("THE PASSWORD IS THE SAME");
+    } else {
+    alert("PASSWORD SHOULD BE EQUAL");
+  }
+}
